5271ef44493747b72116355bdff9705963a4e19b
beshoynaser <beshoynaser012@gmail.com>
2026-07-17 22:46:24 +0300

    Update About section and styles

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -4,25 +4,25 @@
       id: 1,
       year: "1856",
       title: "Birth of Nikola Tesla",
-      description: "Nikola Tesla was born in Smiljan.",
+      description: " he was born in Smiljan.",
     },
     {
       id: 2,
       year: "1884",
       title: "Moved to America",
-      description: "Tesla moved to the United States to pursue his ideas.",
+      description: "Tesla moved to the United States to pursue his ideas which are The Tesla coil Wireless , power experiments , Radio technology and Remote control ",
     },
     {
       id: 3,
       year: "1891",
       title: "Tesla Coil",
-      description: "Tesla invented the famous Tesla Coil.",
+      description: "A Tesla coil is a resonant transformer that generates extremely high-voltage, high-frequency AC electricity, producing dramatic electrical discharges.",
     },
     {
       id: 4,
       year: "1943",
       title: "The End of an Era",
-      description: "Nikola Tesla died in New York City.",
+      description: "Nikola Tesla died in New York City after he made abrilliant inventions and wrote his own history for good.",
     },
   ];
 
